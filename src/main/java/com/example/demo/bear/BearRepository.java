39442eac81f6a38afe73da0d913ccfd639981f9a
Ramon <ramonsaturnino42@gmail.com>
2025-10-08 20:49:38 -0400

    secured keys

---
+++ src/main/java/com/example/demo/bear/BearRepository.java
@@ -0,0 +1,17 @@
+package com.example.demo.bear;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;  
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BearRepository extends JpaRepository<Bear, Long> {
+    List<Bear> findByBearName(String bearName);
+
+    @Query(value = "select * from bears where habitat = :habitat", nativeQuery = true)
+    List<Bear> findByHabitat(String habitat);
+
+
+}
